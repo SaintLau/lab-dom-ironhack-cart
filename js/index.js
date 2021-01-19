68b94cf4ee18ac45e4003ecdb670ebdcb2b9afd5
Laura Santos <alsr16p@gmail.com>
2021-01-19 20:58:15 +0000

    miss link js with html total

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
+function updateSubtotal(product) {  //ALREADY THE FUNCTION
+  let price = product.querySelector('.price span'); 
+  let priceValue = price.innerHTML;
+  
+  
+  let quantity = product.querySelector('.quantity input');
+  let quantityValue = quantity.value; 
+
+   //empty array to have the price after result calculated, what will appear on the box
+  let result = (priceValue * quantityValue);
+
+
+
+  let subtotal = product.querySelector('.subtotal span'); //Hold the value
+  subtotal.innerHTML = result;
+
   console.log('Calculating subtotal, yey!');
+  return result;
 
   //... your code goes here
 }
@@ -9,15 +25,37 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  let products = document.getElementsByClassName('product');
 
+  let productsArray =[...products];
+
+  let total = 0;
+
+  //span.appendChild(total);
+
+  productsArray.forEach(element => {
+    total += updateSubtotal(element);
+     // element.appendChild(span);
+    
+
+
+
+  })
+
+
+
+console.log(total);
   // ITERATION 3
-  //... your code goes here
+  //... your code goes here: is the element iteration abova
+    
+ 
+ 
 }
 
 // ITERATION 4
